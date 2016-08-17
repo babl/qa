8bda89e3ceedf589ffcdc098344c2d9d7618f895
Nelson Neves <nelson.s.neves@gmail.com>
2016-08-17 16:10:06 +0100

    avoid to process kafka logs.qa messages with empty rid

--- kafkalogs/kafka-logs.go
+++ kafkalogs/kafka-logs.go
@@ -15,8 +15,7 @@ type QALog struct {
 func ListenToLogsQA(client *sarama.Client, topic string, chQALog chan *QALog) {
 	log.Debug("Consuming from topic: ", topic)
 	ch := make(chan *kafka.ConsumerData)
-	//go kafka.Consume(client, topic, ch, kafka.ConsumerOptions{Offset: sarama.OffsetOldest})
-	go kafka.Consume(client, topic, ch)
+	go kafka.Consume(client, topic, ch, kafka.ConsumerOptions{Offset: sarama.OffsetNewest})
 	for msg := range ch {
 		log.WithFields(log.Fields{"key": msg.Key}).Debug("QA message received")
 
@@ -24,17 +23,18 @@ func ListenToLogsQA(client *sarama.Client, topic string, chQALog chan *QALog) {
 		qamdata := QAMetadata{}
 		err1 := qamdata.UnmarshalJSON(msg.Value)
 		Check(err1)
-		//qamdata.Debug()
+		//qamdata.DebugY()
 
 		// parse low level log message (logstash "message" property)
 		qamsg := QAMessage{}
 		err2 := qamsg.UnmarshalJSON([]byte(qamdata.Y["message"].(string)))
 		Check(err2)
-		//qamsg.Debug()
+		//qamsg.DebugZ()
 
-		qalog := QALog{qamdata, qamsg}
-		go func() { chQALog <- &qalog }()
-
+		if qamsg.RequestId > 0 {
+			qalog := QALog{qamdata, qamsg}
+			go func() { chQALog <- &qalog }()
+		}
 		msg.Processed <- true
 	}
 	panic("listenToQAMessages: Lost connection to Kafka")
