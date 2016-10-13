cdbbeea07f457e4ddf2f3d17337e370c22330cf4
Nelson Neves <nelson.s.neves@gmail.com>
2016-10-13 19:10:00 +0100

    tweak logstash config for v5 cluster

--- DEVENV/logstash_start.sh
+++ DEVENV/logstash_start.sh
@@ -3,10 +3,10 @@
 docker stop logstash
 docker rm logstash
 
-docker pull registry.babl.sh/logstash:logmatic-v2
+docker pull registry.babl.sh/logstash:logmatic-v4
 
 docker run -d --name logstash \
   -p 12300:12300 \
   -p 12500:12500 \
-  -v "$PWD/logstash.conf:/etc/logstash.conf" \
-  registry.babl.sh/logstash:logmatic-v2
+  -v "$PWD/DEVENV/logstash.conf:/config-dir/logstash.conf" \
+  registry.babl.sh/logstash:logmatic-v4 -f /config-dir/logstash.conf --auto-reload
